4a3c2203970e04764373a1b528ff9288e5efb7e8
CherifiMi <cherifi154949@gmail.com>
2023-09-19 22:04:23 +0100

    spalch screen

--- app/src/main/java/com/example/unnamedai/ui/StartScreen/Components/SplachScreen.kt
+++ app/src/main/java/com/example/unnamedai/ui/StartScreen/Components/SplachScreen.kt
@@ -1,8 +1,22 @@
 package com.example.unnamedai.ui.StartScreen.Components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.example.unnamedai.R
 
 @Composable
 fun SplachScreen() {
-
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black), contentAlignment = Alignment.Center
+    ){
+        Image(painter = painterResource(id = R.drawable.ic_unamed_ui), contentDescription = null)
+    }
 }
