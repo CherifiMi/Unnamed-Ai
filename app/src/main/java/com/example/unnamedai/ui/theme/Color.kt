b946e74176c075a630055727f9dbf13277a81dc2
CherifiMi <cherifi154949@gmail.com>
2023-09-19 21:47:52 +0100

    app icon theme colors

--- app/src/main/java/com/example/unnamedai/ui/theme/Color.kt
+++ app/src/main/java/com/example/unnamedai/ui/theme/Color.kt
@@ -6,3 +6,18 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+val Black = Color(0xFF_171818)
+val White = Color(0xFF_FFFFFF)
+val Blue = Color(0xFF_0183FF)
+
+val Border = Color(0xFF_DCDCE1)
+val BG = Color(0xFF_F2F2F2)
+val ActionBG = Color(0xFF_F5F5F5)
+val Grey = Color(0xFF_5A5B63)
+
+val Red = Color(0xFF_EB3E17)
+val Yellow = Color(0xFF_E9970A)
+val Green = Color(0xFF_18AE81)
+val Input = Color(0xFF_303030)
