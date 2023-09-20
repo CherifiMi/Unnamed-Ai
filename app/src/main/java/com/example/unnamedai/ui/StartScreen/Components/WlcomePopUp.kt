eccfa83a109e49286100707f0b9eb1309b6eb7c3
CherifiMi <cherifi154949@gmail.com>
2023-09-20 02:18:28 +0100

    wlcm screen done

--- app/src/main/java/com/example/unnamedai/ui/StartScreen/Components/WlcomePopUp.kt
+++ app/src/main/java/com/example/unnamedai/ui/StartScreen/Components/WlcomePopUp.kt
@@ -1,17 +1,84 @@
 package com.example.unnamedai.ui.StartScreen.Components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.unnamedai.R
 import com.example.unnamedai.theme.*
+import com.example.unnamedai.ui.setterVisibility
 
 @Composable
-fun WlcomePopUp(modifier: Modifier = Modifier) {
-    Column(modifier.height(300.dp).fillMaxWidth().background(Red)) {
+fun WelcomePopUp(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .background(White, RoundedCornerShape(topEndPercent = 10, topStartPercent = 10)),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 40.dp, bottom = 20.dp)
+                .fillMaxWidth(),
+            text = "Welcome to " + stringResource(id = R.string.app_name),
+            fontFamily = abel,
+            fontSize = 28.sp,
+            color = Black,
+        )
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp)
+                .padding(horizontal = 30.dp),
+            textAlign = TextAlign.Center,
+            text =
+            buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Grey)) {
+                    append("Read our ")
+                }
+                withStyle(style = SpanStyle(color = Blue)) {
+                    append("Privacy Policy")
+                }
+                withStyle(style = SpanStyle(color = Grey)) {
+                    append(". Tap ”Get Started” to accept the ")
+                }
+                withStyle(style = SpanStyle(color = Blue)) {
+                    append("Terms of Service.")
+                }
+            },
+            lineHeight = 21.sp,
+            fontFamily = abel,
+            fontSize = 16.sp,
+            color = Black,
+        )
+
+        UnnamedButton(text = "Get Started") {
+            setterVisibility.value = true
+        }
+
+        Spacer(modifier = Modifier.size(34.dp))
 
     }
 }
--- app/src/main/java/com/example/unnamedai/ui/StartScreen/StartScreen.kt
+++ app/src/main/java/com/example/unnamedai/ui/StartScreen/StartScreen.kt
@@ -1,49 +1,61 @@
 package com.example.unnamedai.ui
 
-import android.widget.GridLayout
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.example.unnamedai.showChatScreen
 import com.example.unnamedai.ui.StartScreen.Components.ChatSetter
 import com.example.unnamedai.ui.StartScreen.Components.SplachScreen
-import com.example.unnamedai.ui.StartScreen.Components.WlcomePopUp
+import com.example.unnamedai.ui.StartScreen.Components.WelcomePopUp
 
 
-
-var wlcVisibility  = mutableStateOf(false)
-var setterVisibility  = mutableStateOf(false)
+var wlcVisibility = mutableStateOf(false)
+var setterVisibility = mutableStateOf(false)
 
 
 @Composable
 fun StartScreen(modifier: Modifier = Modifier) {
 
 
-
-    Box(modifier = Modifier.fillMaxSize()){
+    Box(modifier = Modifier.fillMaxSize()) {
 
         Column(modifier.fillMaxSize()) {
 
-            SplachScreen(Modifier.weight(.7f).clickable { wlcVisibility.value = true })
+            SplachScreen(
+                Modifier
+                    .weight(.7f)
+                    .pointerInput(Unit) {
+                        detectVerticalDragGestures(onDragEnd = { wlcVisibility.value = true}) { change, dragAmount -> }
+                    })
 
-            AnimatedVisibility(visible = wlcVisibility.value && !setterVisibility.value) {
+            AnimatedVisibility(
+                modifier = Modifier.clip(
+                    RoundedCornerShape(
+                        topStartPercent = 10,
+                        topEndPercent = 10
+                    )
+                ),
+                visible = wlcVisibility.value && !setterVisibility.value,
+            ) {
 
-                WlcomePopUp(Modifier.weight(.3f).clickable { setterVisibility.value = true })
+                WelcomePopUp(
+                    Modifier
+                        .weight(.3f))
 
             }
         }
@@ -56,7 +68,8 @@ fun StartScreen(modifier: Modifier = Modifier) {
                 with(density) { 300.dp.roundToPx() }
             },
             exit = slideOutVertically() + shrinkVertically() + fadeOut(),
-            visible = setterVisibility.value) {
+            visible = setterVisibility.value
+        ) {
             ChatSetter(Modifier.clickable { showChatScreen.value = true })
         }
 
