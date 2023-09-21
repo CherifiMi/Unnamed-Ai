a475255238bf34523d9250486d94e23533461624
CherifiMi <cherifi154949@gmail.com>
2023-09-21 20:44:59 +0100

    main vm

--- app/src/main/java/com/example/unnamedai/MainActivity.kt
+++ app/src/main/java/com/example/unnamedai/MainActivity.kt
@@ -4,31 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.unnamedai.util.theme.Blue
-import com.example.unnamedai.util.theme.UnnamedAiTheme
 import com.example.unnamedai.ui.ChatScreen.ChatScreen
 import com.example.unnamedai.ui.HistoryScreen.HistoryScreen
 import com.example.unnamedai.ui.StartScreen
+import com.example.unnamedai.util.theme.UnnamedAiTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
--- app/src/main/java/com/example/unnamedai/MainViewModel.kt
+++ app/src/main/java/com/example/unnamedai/MainViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.unnamedai
 
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+
+}
 
 
 //main
