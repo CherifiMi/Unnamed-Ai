bda904dbb8f48e2b11b519ba41e5e0781f469c46
CherifiMi <cherifi154949@gmail.com>
2023-09-21 22:25:58 +0100

    db usecases

--- app/src/main/java/com/example/unnamedai/domain/use_case/remote/AskChatGBT.kt
+++ app/src/main/java/com/example/unnamedai/domain/use_case/remote/AskChatGBT.kt
@@ -5,9 +5,7 @@ import com.example.unnamedai.domain.repository.ApiRepository
 class AskChatGBT(
     private val apiRepository: ApiRepository
 ) {
-
     suspend operator fun invoke(q: String): String{
         return apiRepository.askChatGBT(q)
     }
-
 }
