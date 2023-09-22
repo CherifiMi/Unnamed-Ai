061f2a536ccdf8a7a6826ff03e0734d84469666b
CherifiMi <cherifi154949@gmail.com>
2023-09-22 20:45:56 +0100

    .

--- app/src/main/java/com/example/unnamedai/MainViewModel.kt
+++ app/src/main/java/com/example/unnamedai/MainViewModel.kt
@@ -164,3 +164,39 @@ class MainViewModel @Inject constructor(
         }
     }
 }
+
+
+
+var history = listOf(
+    Convo(
+        you = "SuperMario",
+        them = "Bowser",
+        date = "Today",
+        talk = listOf()
+    ),
+    Convo(
+        you = "UsersName",
+        them = "AiName",
+        date = "02 Feb, 2023",
+        talk = listOf()
+    ),
+    Convo(
+        you = "UsersName2",
+        them = "AiName2",
+        date = "10 Feb, 2023",
+        talk = listOf()
+    ),
+)
+
+
+data class Convo(
+    val you: String,
+    val them: String,
+    val date: String,
+    val talk: List<Message2>,
+)
+
+data class Message2(
+    val from: String,
+    val content: String
+)
