01abf918887650d2daaef4f87a2e210e15826c06
CherifiMi <cherifi154949@gmail.com>
2023-09-22 19:20:07 +0100

    gpt

--- app/src/main/java/com/example/unnamedai/data/remote/ApiRepositoryImp.kt
+++ app/src/main/java/com/example/unnamedai/data/remote/ApiRepositoryImp.kt
@@ -1,15 +1,28 @@
 package com.example.unnamedai.data.remote
 
 import com.example.unnamedai.domain.repository.ApiRepository
+import com.example.unnamedai.util.OPEN_AI_API_KEY
 
-class ApiRepositoryImp(private val caravanApi: UnnamedAiApi): ApiRepository {
+class ApiRepositoryImp(private val caravanApi: UnnamedAiApi) : ApiRepository {
     override suspend fun askChatGBT(question: String): String {
 
+        val chatRequest = ChatRequest(
+            model = "gpt-3.5-turbo",
+            messages = listOf(
+                Message(role = "user", content = "What is the OpenAI mission?")
+            )
+        )
 
-        //caravanApi.chatgbt()
+        val response = caravanApi.askChat(
+            authorization = "Bearer $OPEN_AI_API_KEY",
+            request = chatRequest
+        ).execute()
 
-
-
-        return "$question  $question  $question  $question  $question"
+        return if (response.isSuccessful) {
+            val chatResponse = response.body()
+            chatResponse!!.choices.last().message.content
+        } else {
+            "not working"
+        }
     }
 }
--- app/src/main/java/com/example/unnamedai/data/remote/UnnamedAiApi.kt
+++ app/src/main/java/com/example/unnamedai/data/remote/UnnamedAiApi.kt
@@ -1,12 +1,32 @@
 package com.example.unnamedai.data.remote
 
-import retrofit2.http.GET
+import com.example.unnamedai.domain.model.ChatResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+
+data class ChatRequest(
+    val model: String,
+    val messages: List<Message>
+)
+
+data class Message(
+    val role: String,
+    val content: String
+)
+
+
 
 interface UnnamedAiApi {
 
-    @GET("/")
-    suspend fun chatgbt(){
-
-    }
+    @Headers("Content-Type: application/json")
+    @POST("v1/chat/completions")
+    suspend fun askChat(
+        @Header("Authorization") authorization: String,
+        @Body request: ChatRequest
+    ): Call<ChatResponse>
 
 }
